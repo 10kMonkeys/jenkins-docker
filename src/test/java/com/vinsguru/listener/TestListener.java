0d9d52fbfa80753e970b9d3bebc39651e308a687
Alex Yarm <aliakseiyarmalchuk@gmail.com>
2025-11-13 12:57:35 +0400

    minor changes

--- src/test/java/com/vinsguru/listener/TestListener.java
+++ src/test/java/com/vinsguru/listener/TestListener.java
@@ -16,7 +16,6 @@ public class TestListener implements ITestListener {
         String htmlImageFormat = "<img width=700px src='data:image/png;base64,%s' />";
         String htmlImage = String.format(htmlImageFormat, screenshot);
         Reporter.log(htmlImage);
-        //1
     }
 
 }
